7fb26578cc99c4c277132ed950be6b7e0b0c7c32
realAbitbol <george.abitbol.official@gmail.com>
2025-09-14 08:04:57 +0200

    Add missing dependancy pydantic to requirements

--- start_bot.bat
+++ start_bot.bat
@@ -64,7 +64,7 @@ if exist "requirements.txt" (
                 )
             ) else (
                 echo Warning: Failed to create virtual environment
-                echo You may need to install dependencies manually: aiohttp
+                echo You may need to install dependencies manually: aiohttp, pydantic
                 pause
             )
         )
